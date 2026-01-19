384fba0e95605de5470dbb2ac6be8cf7fb9ca37e
shalini <s.rathore43@gmail.com>
2026-01-19 09:21:15 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import First from "./components/First"
+export default function App() {
+  return (
+      <div>
+        <First></First>
+        <hr />
+        <First></First>
+  </div>
+  )
+}
+
+// export default App
