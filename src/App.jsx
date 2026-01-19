0d1134ba09a71e2dd798a4de28333a0d841844c7
shalini <s.rathore43@gmail.com>
2026-01-19 09:38:46 +0530

    style added

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,10 @@
 import First from "./components/First"
+import Welcome from "./components/Welcome"
+import "./App.css"
 export default function App() {
   return (
       <div>
+        <Welcome></Welcome>
         <First></First>
         <hr />
         <First></First>
---
+++ src/components/Welcome.jsx
@@ -0,0 +1,9 @@
+
+export default function Welcome() {
+  return (
+    // <div style={{color:"red",fontSize:"25px",textAlign:"center"}}>
+   <div className="welcome">
+    Welcome To Ahmedabad
+        </div>
+  )
+}
