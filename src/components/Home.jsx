a029ffa5ffa82c0e2195437087916c5ce609dda4
shalini <s.rathore43@gmail.com>
2026-01-21 09:42:04 +0530

    khgsd

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,7 +1,9 @@
 import React from 'react'
-
+import Girl from "../assets/Girlbanner.webp"
 export default function Home() {
   return (
-    <div>Home</div>
+    <div>Home
+      <img src={Girl} alt="" />
+    </div>
   )
 }
