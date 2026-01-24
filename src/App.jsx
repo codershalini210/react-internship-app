a6eefd693e078965e15dcaad2ab436e44a0ccb2d
shalini <s.rathore43@gmail.com>
2026-01-24 08:54:56 +0530

    sdfsdf

--- src/App.jsx
+++ src/App.jsx
@@ -12,10 +12,41 @@ import Footer from "./components/Footer"
 import EventHandling from "./components/EventHandling"
 import StateEg1 from "./components/StateEg1"
 import Calculator from "./components/Calculator"
+import { useState } from "react"
 export default function App() {
+  //
+  const [colorname,setcolorname] = useState("black")
+  const [fontcolor,setfontcolor] = useState("white")
+  let  changeTheme=(bgcolor,color)=>
+  {
+    setcolorname(bgcolor)
+    setfontcolor(color)
+
+  }
   return (
       <> 
-      <Menubar></Menubar>
+      <div style={{textAlign:'right',backgroundColor:colorname}}>
+        <input type="button"
+         style={{height:'30px',width:'30px',backgroundColor:'black',color:'white'}} 
+          value="A"
+          onClick={()=>changeTheme('black','white')}/>
+          
+          <input type="button"  onClick={()=>changeTheme('yellow','black')}
+         style={{height:'30px',width:'30px',backgroundColor:'yellow',color:'black'}} 
+          value="A"/>
+
+          <input type="button"
+           onClick={()=>changeTheme('blue','white')}
+         style={{height:'30px',width:'30px',backgroundColor:'blue',color:'white'}} 
+          value="A"/>
+
+          <input type="button"
+           onClick={()=>changeTheme('yellowgreen','black')}
+         style={{height:'30px',width:'30px',backgroundColor:'yellowgreen',color:'black'}} 
+          value="A"/>
+      </div>
+      <Menubar bgcolor={colorname} fontcolor={fontcolor}></Menubar>
+      <div style={{color:fontcolor,backgroundColor:colorname}}>
       <Routes>
         <Route path="StateEg1" element={<StateEg1></StateEg1>}>State eg1</Route>
         <Route path="/EventHandling" element={<EventHandling></EventHandling>}></Route>
@@ -26,6 +57,7 @@ export default function App() {
     <Route path="/calc" element={<Calculator></Calculator>}></Route>
       </Routes>
       <Footer></Footer>
+      </div>
   </>
   )
 }
--- src/components/Menubar.jsx
+++ src/components/Menubar.jsx
@@ -1,17 +1,17 @@
 
 import { Link } from "react-router-dom"
-export default function Menubar() {
+export default function Menubar(props) {
   return (
     <>
     
- <ul className="menu-bar">
-    <li><Link to="/">Home</Link> </li>
-    <li> <Link to="/services">Services</Link></li>
-    <li> <Link to="/team">Team</Link></li>
-    <li> <Link to="/contact">Contact</Link></li>
-    <li><Link to="/EventHandling">Event EG</Link></li>
-    <li> <Link to="/stateeg1">State eg1</Link></li>
-    <li> <Link to="/calc">Calculator</Link></li>
+ <ul className="menu-bar" style={{color:props.fontcolor,backgroundColor:props.bgcolor}}>
+    <li><Link to="/" style={{color:props.fontcolor}}>Home</Link> </li>
+    <li> <Link to="/services" style={{color:props.fontcolor}}>Services</Link></li>
+    <li> <Link to="/team" style={{color:props.fontcolor}}>Team</Link></li>
+    <li> <Link to="/contact" style={{color:props.fontcolor}}>Contact</Link></li>
+    <li><Link to="/EventHandling" style={{color:props.fontcolor}}>Event EG</Link></li>
+    <li> <Link to="/stateeg1" style={{color:props.fontcolor}}>State eg1</Link></li>
+    <li> <Link to="/calc" style={{color:props.fontcolor}}>Calculator</Link></li>
  </ul>
     </>
   )
